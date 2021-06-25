c8efe1af407b622044d4842e886698ea2354ded6
whoraks <whoraks@gmail.com>
2021-06-25 15:45:16 +0900

    default

--- index.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var path_1 = require("path");
-console.log(path_1.dirname(__filename));
-console.log(path_1.basename(__filename));
--- index.ts
+++
@@ -1,4 +0,0 @@
-import { dirname, basename } from 'path';
-
-console.log(dirname(__filename));
-console.log(basename(__filename));
